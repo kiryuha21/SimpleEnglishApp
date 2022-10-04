f8ac583ee79b4e8ad865f36e0b5af028e3df9051
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2022-10-04 13:35:31 +0300

    continue doing registration

--- app/src/main/java/com/example/simple_english/MainActivity.kt
+++ app/src/main/java/com/example/simple_english/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button = findViewById<Button>(R.id.SignUpButton)
+        val button = findViewById<Button>(R.id.SignUpButton)
         button.setOnClickListener {
             val signUpIntent = Intent(this, SignUp::class.java)
             startActivity(signUpIntent)
