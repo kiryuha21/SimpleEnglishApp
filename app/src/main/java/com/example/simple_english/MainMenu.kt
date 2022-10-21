d38e1ade60b45b5aec239ad72493b775fd9f0dc8
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2022-10-21 21:41:47 +0300

    less raw main menu

--- app/src/main/java/com/example/simple_english/MainMenu.kt
+++ app/src/main/java/com/example/simple_english/MainMenu.kt
@@ -2,10 +2,21 @@ package com.example.simple_english
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
+import com.example.simple_english.databinding.ActivityMainMenuBinding
 
 class MainMenu : AppCompatActivity() {
+    lateinit var binding : ActivityMainMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu)
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+    fun onMenuImageClick(view : View) {
+        binding.drawer.openDrawer(GravityCompat.START)
     }
 }
