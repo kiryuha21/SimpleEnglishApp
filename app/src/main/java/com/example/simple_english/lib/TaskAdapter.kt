103fb7ad3fdc3007f6da4bb8eead299b750ba6b7
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2022-12-19 16:52:43 +0300

    some minor comments for code

--- app/src/main/java/com/example/simple_english/lib/TaskAdapter.kt
+++ app/src/main/java/com/example/simple_english/lib/TaskAdapter.kt
@@ -14,24 +14,29 @@ import com.example.simple_english.databinding.TaskItemBinding
 // Adapter for tasks
 class TaskAdapter(private val clickListener: (View) -> Unit) :
     RecyclerView.Adapter<TaskAdapter.TaskHolder>() {
-    var tasks = ArrayList<TaskHeader>()
+    var tasks = ArrayList<TaskHeader>()  // main array with tasks
 
+    // creation of task holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
         val view = LayoutInflater.from(parent.context).inflate(layout.task_item, parent, false)
         return TaskHolder(view)
     }
 
+    // for every binded element
     override fun onBindViewHolder(holder: TaskHolder, position: Int) {
         holder.bind(tasks[position], position, clickListener)
     }
 
+    // number of elements in main array
     override fun getItemCount(): Int {
         return tasks.size
     }
 
+    // class for every binded item
     class TaskHolder(item: View) : RecyclerView.ViewHolder(item) {
         private val binding = TaskItemBinding.bind(item)
 
+        // custom function to handle binding
         fun bind(task: TaskHeader, position: Int, clickListener: (View) -> Unit) = with(binding) {
             studyType.setImageResource(
                 when (task.taskType) {
